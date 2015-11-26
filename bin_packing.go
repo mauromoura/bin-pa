2d4a15613212e1fc77e8ec2531fdb7ff38b76218
mauromoura <mauro.moura.7@gmail.com>
2015-11-25 23:48:08 -0200

    added recursive algorithm

--- bin_packing.go
+++ bin_packing.go
@@ -1,29 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	values := []int{1, 2, 3, 4}
-	permute(4, values)
+	//weights := []int{7, 7, 3, 4}
+	//bin := 7
+	weights := []int{5, 7, 5, 2, 4, 2, 5, 1, 6}
+	bin := 10
+	binPackingRecursive(weights, bin)
 }
 
-func binPacking(weights []int, bin int) {
-
+func binPackingRecursive(weights []int, bin int) {
+	min := math.MaxInt32
+	permute(len(weights), weights, &min, bin)
+	fmt.Println(min)
 }
 
-func permute(n int, A []int) {
+func bruteForce(A []int, bin int, min *int) {
+	count := 1
+	binAux := bin
+	for i := 0; i < len(A); i++ {
+		if A[i] <= binAux {
+			binAux -= A[i]
+		} else {
+			binAux = bin
+			binAux -= A[i]
+			count++
+		}
+	}
+	if count < *min {
+		*min = count
+		fmt.Println(A, count)
+	}
+}
+
+func permute(n int, A []int, min *int, bin int) {
 	if n == 1 {
-		fmt.Println(A)
+		bruteForce(A, bin, min)
 	} else {
 		for i := 0; i < n-1; i++ {
-			permute(n-1, A)
+			permute(n-1, A, min, bin)
 			if n%2 == 0 {
 				swap(A, i, n-1)
 			} else {
 				swap(A, 0, n-1)
 			}
 		}
-		permute(n-1, A)
+		permute(n-1, A, min, bin)
 	}
 }
 
